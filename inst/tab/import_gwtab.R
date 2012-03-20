27fa4fb9e93339c0f3e86d0c95a5a34e9a3dbe87
Vince Carey <stvjc@channing.harvard.edu>
2012-03-20 16:45:05 +0000

    compressing and adding import utility
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/gwascat@64157 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ inst/tab/import_gwtab.R
@@ -0,0 +1,19 @@
+import_gwtab = function(fn) {
+ #
+ # shouldn't be necessary but there are odd phenomena in the
+ # distributed file
+ #
+ recs = readLines(fn)
+ nc = nchar(recs)
+ if (any(nc==0)) recs = recs[-which(nc==0)] # shld drop last
+ fhead = strsplit(recs[1], "\t")[[1]]
+ dat = strsplit(recs[-1], "\t")
+ nrec = length(dat)
+ nfields = length(fhead)
+ nfdat = sapply(dat, length)
+ if (length(table(nfdat)) > 1) stop("irregular field number in records, sorry.")
+ outdat = matrix("", nr=nrec, nc=nfields)
+ for (i in 1:nrec) outdat[i,] = dat[[i]]
+ colnames(outdat) = fhead
+ data.frame(outdat, stringsAsFactors=FALSE)
+}
