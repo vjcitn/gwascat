5e54e8321e7483b48452cdb368448235f74af21e
Vince Carey <stvjc@channing.harvard.edu>
2012-06-02 14:25:55 +0000

    some new utilities
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/gwascat@66360 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ R/descendants.R
@@ -0,0 +1,41 @@
+
+descendants = function(g, root, depth=1) {
+ #
+ # return vector of nodes at distance at most depth from root
+ # counting edges (not weights)
+ # include root
+ #
+ alln = list()
+ alln[[1]] = root
+ for (i in 1:depth) 
+   alln[[i+1]] = unlist(adj(g, unlist(alln)))
+ unique(unlist(alln))
+}
+
+parentingDF = function(gnel, nodes, useNodeData=NULL) {
+ if (!is(gnel, "graphNEL")) stop("currently for graphNEL input")
+ ng = subGraph(nodes, gnel)
+ mapperf = function(x)x
+ mapper = NULL
+ if (!is.null(useNodeData)) {
+   if (length(useNodeData)>1 | !is.character(useNodeData)) stop("useNodeData must be atomic character")
+   nd = nodeData(ng, , useNodeData)
+   mapper = unlist(nd)
+   names(mapper) = names(nd)
+   mapperf = function(x) mapper[x]
+ }
+ ng = as(ng, "graphBAM")
+ ftmat = extractFromTo(ng)  # data.frame
+ ftmat = lapply(ftmat[1:2], as.character)
+ ftmat = do.call(cbind, ftmat)
+ formal = apply(ftmat, 1, function(x) paste(x, collapse="_"))
+ colnames(ftmat)[1] = "Parent"
+ dd = dim(ftmat)
+ dn = dimnames(ftmat)
+ x = mapperf( as.character(ftmat) )
+ mx = matrix(x, nrow=dd[1])
+ dimnames(mx) = dn
+ data.frame(cbind(mx, formal))
+}
+ 
+
---
+++ R/traitsManh.R
@@ -0,0 +1,16 @@
+traitsManh = function( gwr, 
+   selr=GRanges(seqnames="chr17", IRanges(3e7, 5e7)), 
+   traits = c("Asthma", "Parkinson's disease", "Height", "Crohn's disease"),
+   truncmlp = 25,
+   ...)
+ {
+ require(ggbio)
+ gwr = gwr[ which(gwr %in% selr) ]
+ availtr = values(gwr)$Disease.Trait
+ oth = which(!(availtr %in% traits))
+ availtr[oth] = "Other"
+ values(gwr)$Trait = availtr
+ pv = values(gwr)$Pvalue_mlog 
+ values(gwr)$Pvalue_mlog = ifelse(pv > 25, 25, pv)
+ autoplot( gwr, geom="point", aes(y=Pvalue_mlog, color=Trait))
+}
