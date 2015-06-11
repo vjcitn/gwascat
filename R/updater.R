07d489153bf0c2c5f992a829253664373087c783
Vince Carey <stvjc@channing.harvard.edu>
2015-06-11 17:57:34 +0000

    handle GRCh37 liftover
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/gwascat@104836 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/updater.R
+++ R/updater.R
@@ -16,23 +16,21 @@ makeCurrentGwascat.legacy = function(table.url="http://www.genome.gov/admin/gwas
 
 makeCurrentGwascat = function(table.url=
   "http://www.ebi.ac.uk/gwas/api/search/downloads/alternative",
-   fixNonASCII=FALSE, useHg38seqinfo = TRUE, altSeqinfo, withOnt=FALSE) {
+   fixNonASCII=FALSE, genome="GRCh38", withOnt=FALSE) {
+ stopifnot(genome %in% c("GRCh37", "GRCh38"))
  suppressWarnings({
  if (!withOnt) table.url = sub("alternative", "full", table.url)
  message(paste0("running read.delim on ", table.url, "..."))
  tab = read.delim(url(table.url), sep="\t", header=TRUE, check.names=FALSE, stringsAsFactors=FALSE)
  })
  message(paste0("formatting gwaswloc instance..."))
- if (missing(altSeqinfo) & !useHg38seqinfo) stop("need an altSeqinfo when  useHg38seqinfo is FALSE")
  if (fixNonASCII) tab = fixNonASCII(tab)
  cur = gwdf2GRanges(tab, extractDate=as.character(Sys.Date()))
  seqlevelsStyle(cur) = "NCBI"
  cursn = seqlevels(cur)
- if (useHg38seqinfo) {
-    data(si.hs.38)  
-    seqinfo(cur) = si.hs.38[cursn]
-    }
- else seqinfo(cur) = altSeqinfo[cursn]
+ data(si.hs.38)  
+ seqinfo(cur) = si.hs.38[cursn]
+ if (genome == "GRCh37") cur = lo38to19(cur)
  metadata(cur) = list(
     date.created = date(),
     creation = match.call(),
@@ -41,3 +39,27 @@ makeCurrentGwascat = function(table.url=
  message("done.")
  cur
 }
+
+lo38to19 = function(gwwl) {
+ message("starting liftover from GRCh38 to GRCh37")
+ stopifnot(genome(gwwl)[1] == "GRCh38")
+ ah = AnnotationHub()
+ ii = query(ah, "UCSC liftOver chain file from hg38 to hg19")
+ ch = ah[[names(ii)]]
+ seqlevelsStyle(gwwl) = "UCSC"
+ g19 = liftOver( as(gwwl, "GRanges"), ch )
+ message("liftover complete.")
+ e = elementLengths(g19)
+ dr = which(e==0)
+ if (length(dr)>0) g19 = g19[-dr]
+ g19 = unlist(g19)
+ metadata(g19)$conversion = "liftOver"
+ metadata(g19)$sessInfo.creation.liftOver = sessionInfo()
+ seqlevelsStyle(g19) = "NCBI"
+ genome(g19) = "GRCh37"
+ cursn = seqlevels(g19)
+ data(si.hs.37)
+ seqinfo(g19) = si.hs.37[cursn]
+ new("gwaswloc", extractDate=date(), g19)
+}
+
